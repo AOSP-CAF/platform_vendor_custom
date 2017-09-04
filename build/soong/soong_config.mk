958b8ed037fdc398156b8b879a4e3a124fedcc8e
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put our custom variables in the "custom" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside our custom structure
 custom_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Custom": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
