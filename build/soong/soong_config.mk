50f9632d31325a47aa45cc8f8426a8ff4026825b
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:01:35 -0700

    soong_config: Add TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY variable handling
    Change-Id: I6f1cbcd883e91c8c3724dbe53732c7e777e21657

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,8 @@ custom_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Custom": {'; \
+	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
+	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
